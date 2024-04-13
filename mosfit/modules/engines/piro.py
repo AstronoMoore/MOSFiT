92b925b7ceb6f6232d665cd195a603e2e359d287
AstronoMoore <tom.moore017@gmail.com>
2024-04-13 14:49:21 -0400

    updated latex names

--- mosfit/modules/engines/piro.py
+++ mosfit/modules/engines/piro.py
@@ -27,6 +27,13 @@ class Shock_Cooling(Engine):
 
     def process(self, **kwargs):
 
+        def shock_cooling_luminosity(L_0, td, n, t):
+            if t <= td:
+                return L_0 * ( (td / t) ** (4. / (n - 2)))
+            if t > td:
+                return L_0 * np.exp(-0.5 * ((t ** 2) / (td ** 2.)  - 1.))
+
+
         """Process module."""
         self._times = kwargs[self.key('dense_times')]
         self._rest_t_explosion = kwargs[self.key('texplosion')]
@@ -37,6 +44,8 @@ class Shock_Cooling(Engine):
         self._r_init = kwargs[self.key('rinit')]
         self._n = kwargs[self.key('n')]
         self._delta = kwargs[self.key('delta')]
+
+
 
         E_SN =  0.3 * self._m_ejecta * self._v_ejecta**2 # This should really be the photospheric velocity 
         E51 = E_SN / 1e51
@@ -51,12 +60,6 @@ class Shock_Cooling(Engine):
             for x in self._times
         ]
         ts = np.array(ts)
-
-        def shock_cooling_luminosity(L_0, td, n, t):
-            if t <= td:
-                return L_0 * ( (td / t) ** (4. / (n - 2)))
-            if t > td:
-                return L_0 * np.exp(-0.5 * ((t ** 2) / (td ** 2.)  - 1.))
             
         L_0 = ((np.pi * (self._n - 1.)) / (3. * (self._n - 5.))) * ((C_CGS * self._r_init * vt**2. )/(self._kappa))
             
